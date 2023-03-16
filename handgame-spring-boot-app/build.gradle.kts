a21208ba2cf162fd539f32b782c4119bc0fd7997
Hooeats <hooneats@naver.com>
2023-03-16 19:58:21 +0900

    빌드 후 실행보다 빌드 한번으로 실행되도록 api추가

--- handgame-spring-boot-app/build.gradle.kts
+++ handgame-spring-boot-app/build.gradle.kts
@@ -2,5 +2,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
 
     // 우리가 만든 커스텀 스프링 부트 스타터
-    implementation("com.fastcampus.springboot:handgame-spring-boot-starter:1.0-SNAPSHOT")
+//    implementation("com.fastcampus.springboot:handgame-spring-boot-starter:1.0-SNAPSHOT")
+    api(project(":handgame-spring-boot-starter"))
 }
